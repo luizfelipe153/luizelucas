278c168a70f2e64552138b63c4fb1d5bc988c03a
luiz felipe <126171781+luizfelipe153@users.noreply.github.com>
2024-03-15 09:46:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const botoes = Document 
+const botoes = Document.querySelectorAll (".botao");
+const textos = document.querySelectorAll (".aba-conteudo");
+
+for (let i = 0; i <botoes.length; i++) {
+botoes[i].onclick = function () 
+
+for (let j = 0; j < botoes.length; j++) {
+botoes [j].classlist.remove("ativo");
+textos[j].classlist.remove("ativo");  
+}
+}
+
