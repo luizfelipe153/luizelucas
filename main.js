e02c8229f1690a7af3a42481bd743137785681dc
luiz felipe <126171781+luizfelipe153@users.noreply.github.com>
2024-08-30 08:51:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,100 +1,77 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
-const numeros = '0123456789';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeross = '0123456789';
 const simbolos = '!@%*?';
-const botoes = document.querySelectorAll('.parametro-senha__botao');
+const botoes = document.querySelectorAll('.parametro-senha_botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
 
-botoes[0].onclick = diminuiTamanho;
-botoes [1].onclick = aumentaTamanho;
+botoes[0].onclick = diminuitamanho;
+botoes[1].onclick = aumentatamanho;
 
-function diminuiTamanho() {
+function diminuitamanho(){
     if (tamanhoSenha > 1){
-        // tamanhoSenha = tamanhosenha-1;
-        tamanhoSenha--;
+        //tamanhoSenha =tamanhoSenha-1;
+          tamanhoSenha--;
+    }      
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
-numeroSenha.textContent = tamanhoSenha;
-geraSenha();
-}
-function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
-        // tamanhoSenha = tamanhoSenha+1;
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        //tamanhoSenha = tamanhoSenh+1;
         tamanhoSenha++;
     }
+
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 
-for (i = 0; i < checkbox.length; i++) {
-    checkbox[i].onclick = geraSenha;
-}
+    for (i = 0; 1 < checkbox.length; i++) {
+        checkbox[i].onclick = geraSenha
+    }
 
-geraSenha();
+    geraSenha();
 
-function geraSenha() {
+function geraSenha(){
     let alfabeto = '';
     if (checkbox[0].checked) {
-        alfabeto = alfabeto + letrasMinusculas;
+        alfabeto = alfabeto + letrasMaiusculas;
     }
-    if (checkbox[1].checked) {
-        alfabeto = alfabeto + letrasMinusculas;
+   if (checkbox[1].checked) {
     }
     if (checkbox[2].checked) {
-        alfabeto = alfabeto + numeros;
+        alfabeto = alfabeto = numeros;
     }
     if (checkbox[3].checked) {
-        alfabeto = alfabeto + simbolos;
-    }
-    let senha = '';
-    for (let i = 0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random() * alfabeto.length;
-        numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + alfabeto[numeroAleatorio];
-    }
-    campoSenha.value = senha;
-    classificasenha
-}
-const contadores = document.querySelectorAll(".contaor");
-const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-
-
-function calculaTempo(tempoObjetivo) {
-let tempoAtual = new Date();
-let tempoFinal = tempoObjetivo - tempoAtual;
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24); 
-
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-if (tempoFinal > 0){
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    } else {
-        return "Prazo Finalizado";
+        alfabeto = alfabeto = simbolos;
     }
 }
-
-function atulizaCronometro(){
-    for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+let senha = '';
+for (let i = 0; i < tamanhoSenha; i++) {
+    let numeroAleatorio = Math.random() * alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
 }
-}
+campoSenha.value = senha;
+classificaSenha(alfabeto.lenght);
 
-function comecaCronometro(){
-    atulizaCronometro();
-    setInterval(atulizaCronometro,1000);
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35) {
+        forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar ate" + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para"
 
-    comecaCronometro();
+}
